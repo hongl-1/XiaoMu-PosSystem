21d24ff6672e63c8bb4c24341e8c2be2a927a401
hongliang <hongl_gitee@163.com>
2022-09-17 23:22:58 +0800

    Update cash.js

--- client/src/redux/reduces/cash.js
+++ client/src/redux/reduces/cash.js
@@ -48,7 +48,6 @@ const orderInit = {
 }
 
 function currentOrder(state = orderInit, action) {
-  console.log(state)
   switch (action.type) {
     case CASH_HISTORY_ORDER_IMPORT:
       return (() => {
